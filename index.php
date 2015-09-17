12f614902235da434495ea0dd74e4b405bd8d34b
mteixeira <marcelo.t80@gmail.com>
2015-09-17 15:29:51 +0100

    init code

---
+++ index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Mayra Escort porto constituição - 916021956</title>
+
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Mayra escort Porto - Mayra escort e massagista profissional porto. Erica escort no porto. Erica Sexy">
+    <meta name="author" content="teixeira">
+    <meta name="keywords"
+          content="916021956, Mayra porto, Mayra escort, erica, porto sexy, portomassagens,massagens sensuais, massagens eroticas, massagem body body, massagem a 4mãos, massagem tantrica, massoterapia sensorial, happy ending, descompressão, relax, massagens de relaxamento, ">
+    <meta name="generator" content="HubSpot">
+    <link rel="shortcut icon" href="//cdn2.hubspot.net/hub/53/file-8149778-png/fav.png?t=1440147094131">
+    <meta property="og:description" content="916021956 escort porto">
+    <!-- Included Bootstrap CSS Files -->
+    <link rel="stylesheet" href="./js/bootstrap/css/bootstrap.min.css" />
+    <!-- Just include this Css file -->
+    <link rel="stylesheet" href="./css/jquery.carousel.fullscreen.css" />
+
+</head>
+<body>
+
+<!-- This is a typical Twitter Bootstrap Carousel -->
+<!-- Carousel -->
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+        <ol class="carousel-indicators">
+            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="4"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="5"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="6"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="7"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="8"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="9"></li>
+        </ol>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+        <div class="item active">
+            <img src="./img/1.jpg" alt="" />
+            <div class="container">
+                <div class="carousel-caption">
+                    <h1 style="color:white">Mayra | 916 021 956</h1>
+                    <p class="lead" style="color:honeydew">Morena sensual meiga e atrevida. Completissima atende cavaleiros de bom gosto que sem o que querem. Adoro um oral profundo e 69 delirante! até as 21 de segunda a sabado.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="item">
+            <img src="./img/2.jpg" alt="" />
+            <div class="container">
+                <div class="carousel-caption">
+                    <h1 style="color:white">Mayra | 916 021 956</h1>
+                    <p class="lead" style="color:honeydew">Morena sensual meiga e atrevida. Completissima atende cavaleiros de bom gosto que sem o que querem. Adoro um oral profundo e 69 delirante! até as 21 de segunda a sabado.</p>
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <img src="./img/3.jpg" alt="" />
+            <div class="container">
+                <div class="carousel-caption">
+                    <h1 style="color:white">Mayra | 916 021 956</h1>
+                    <p class="lead" style="color:honeydew">Morena sensual meiga e atrevida. Completissima atende cavaleiros de bom gosto que sem o que querem. Adoro um oral profundo e 69 delirante! até as 21 de segunda a sabado.</p>
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <img src="./img/4.jpg" alt="" />
+            <div class="container">
+                <div class="carousel-caption">
+                    <h1 style="color:white">Mayra | 916 021 956</h1>
+                    <p class="lead" style="color:honeydew">Morena sensual meiga e atrevida. Completissima atende cavaleiros de bom gosto que sem o que querem. Adoro um oral profundo e 69 delirante! até as 21 de segunda a sabado.</p>
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <img src="./img/5.jpg" alt="" />
+            <div class="container">
+                <div class="carousel-caption">
+                    <h1 style="color:white">Mayra | 916 021 956</h1>
+                    <p class="lead" style="color:honeydew">Morena sensual meiga e atrevida. Completissima atende cavaleiros de bom gosto que sem o que querem. Adoro um oral profundo e 69 delirante! até as 21 de segunda a sabado.</p>
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <img src="./img/6.jpg" alt="" />
+            <div class="container">
+                <div class="carousel-caption">
+                    <h1 style="color:white">Mayra | 916 021 956</h1>
+                    <p class="lead" style="color:honeydew">Morena sensual meiga e atrevida. Completissima atende cavaleiros de bom gosto que sem o que querem. Adoro um oral profundo e 69 delirante! até as 21 de segunda a sabado.</p>
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <img src="./img/7.jpg" alt="" />
+            <div class="container">
+                <div class="carousel-caption">
+                    <h1 style="color:white">Mayra | 916 021 956</h1>
+                    <p class="lead" style="color:honeydew">Morena sensual meiga e atrevida. Completissima atende cavaleiros de bom gosto que sem o que querem. Adoro um oral profundo e 69 delirante! até as 21 de segunda a sabado.</p>
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <img src="./img/8.jpg" alt="" />
+            <div class="container">
+                <div class="carousel-caption">
+                    <h1 style="color:white">Mayra | 916 021 956</h1>
+                    <p class="lead" style="color:honeydew">Morena sensual meiga e atrevida. Completissima atende cavaleiros de bom gosto que sem o que querem. Adoro um oral profundo e 69 delirante! até as 21 de segunda a sabado.</p>
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <img src="./img/9.jpg" alt="" />
+            <div class="container">
+                <div class="carousel-caption">
+                    <h1 style="color:white">Mayra | 916 021 956</h1>
+                    <p class="lead" style="color:honeydew">Morena sensual meiga e atrevida. Completissima atende cavaleiros de bom gosto que sem o que querem. Adoro um oral profundo e 69 delirante! até as 21 de segunda a sabado.</p>
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <img src="./img/10.jpg" alt="" />
+            <div class="container">
+                <div class="carousel-caption">
+                    <h1 style="color:white">Mayra | 916 021 956</h1>
+                    <p class="lead" style="color:honeydew">Morena sensual meiga e atrevida. Completissima atende cavaleiros de bom gosto que sem o que querem. Adoro um oral profundo e 69 delirante! até as 21 de segunda a sabado.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Controls -->
+    <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+    </a>
+    <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+    </a>
+</div>
+
+<script src="./js/jquery-1.10.0.min.js"></script>
+<script src="./js/bootstrap/js/bootstrap.min.js"></script>
+
+<!-- Just include this Js file -->
+<script src="./js/jquery.carousel.fullscreen.js"></script>
+</body>
+</html>
